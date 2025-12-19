a0559c4e3836e0dab4b016d7ab07cb880e19dbaf
Camille-lt <camillelebigot5@gmail.com>
2025-12-19 18:16:12 +0100

    created routes crud

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [TaskController::class, 'index']);
+
+//route POST
+Route::post('/tasks', [TaskController::class, 'store']);
+
+//route PATCH
+Route::patch('/tasks/{task}/status', [TaskController::class, 'updateStatus']);
+
+//route DELETE
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy']); // Gère la suppression
